e270abb032fcab16882c3229b6a5c30bef7f5f0f
rafaelone <rafa.sergio.02@live.com>
2025-12-30 12:00:44 -0300

    style: categories pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
 import { DashBoard } from './pages/Dashboard';
 import { Transactions } from './pages/Transactions';
+import { Categories } from './pages/Categories';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/signUp" element={<SignUp />} />
       <Route path="/dashboard" element={<DashBoard />} />
       <Route path="/transactions" element={<Transactions />} />
+      <Route path="/categories" element={<Categories />} />
     </Routes>
   );
 }
